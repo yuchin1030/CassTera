727e37ca58ba4cbfc4708d3f832be07c41c025f2
9YOHAN8 <160392746+9YOHAN8@users.noreply.github.com>
2024-06-04 15:46:20 +0900

    방만들기 세팅2

--- Source/CassTera/CassTera.Build.cs
+++ Source/CassTera/CassTera.Build.cs
@@ -8,6 +8,6 @@ public class CassTera : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Niagara" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Niagara", "OnlineSubsystem", "OnlineSubsystemSteam" });
 	}
 }
